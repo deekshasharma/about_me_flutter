b8f9285d68725017689615f4054321b3ce0aca13
deekshasharma <deeksha.sharma25@gmail.com>
2022-06-13 12:28:15 -0700

    wraps MaterialApp widget inside a stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(AboutMeApp());
+}
+
+class AboutMeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
-        body: Container(),
+        backgroundColor: Colors.greenAccent,
+        body: SafeArea(
+          child: Center(
+            child: Container(
+                color: Colors.white,
+                height: 100.0,
+                width: 100.0,
+                margin: EdgeInsets.all(20.0),
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: Text(
+                  'Hello, Deeksha, how are you doing?',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                )),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
